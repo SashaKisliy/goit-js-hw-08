fda02d9207d50bd4f4127cdd2a1ade0daf2e2a36
Alexander Kisliy <108825919+SashaKisliy@users.noreply.github.com>
2022-11-26 06:41:27 +0200

    complete hw

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,31 @@
 // Add imports above this line
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+const images = document.querySelector('.gallery');
+
+const galleryMarkup = createGalleryItems(galleryItems);
+
+images.insertAdjacentHTML('afterbegin', galleryMarkup);
+
+
+let gallery = new SimpleLightbox('.gallery__item', { 
+    captionsData: 'alt',
+    captionDelay: 250 
+});
+
+
+
+
+function createGalleryItems(galleryItems) {
+    return galleryItems.map(({ preview, original, description }) => 
+    {return `<a class="gallery__item" href="${original}">
+    <img class="gallery__image" src="${preview}" alt="${description}" />
+    </a>`}
+    ).join('');
+}
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,42 @@
+import throttle from "lodash.throttle";
+
+const FEEDBACK = "feedback-form-state";
+let formData = {};
+
+const input = document.querySelector('input');
+const textArea = document.querySelector('textarea')
+const form = document.querySelector('.feedback-form');
+
+populuteForm();
+
+form.addEventListener('input', throttle(inputOnForm, 500));
+form.addEventListener('submit', submitForm)
+
+
+function inputOnForm(e) {
+    
+    formData[e.target.name] = e.target.value;
+
+    const dataString = JSON.stringify(formData);
+
+
+    localStorage.setItem(FEEDBACK, dataString);
+    console.log(dataString);
+}
+
+function submitForm(e) {
+    e.preventDefault();
+    e.target.reset();
+    localStorage.removeItem(FEEDBACK);
+}
+
+function populuteForm() {
+    const savedForm = JSON.parse(localStorage.getItem(FEEDBACK));
+    if (savedForm) {
+        console.log(savedForm);
+        input.value = savedForm.email;
+        textArea.value = savedForm.message;
+    }
+}
+
 
